91e0fae032ab1e6031a5ce4c0ebe26dd0e7df01e
@micahstubbs <micah.stubbs@gmail.com>
2018-05-29 16:24:46 -0700

    abstract out selectCircularLinkTypes

--- dist/d3-sankey-circular.mjs
+++ dist/d3-sankey-circular.mjs
@@ -829,6 +829,51 @@ function createsCycle(originalSource, nodeToCheck, graph, id) {
   return false;
 }
 
+// Assign a circular link type (top or bottom), based on:
+// - if the source/target node already has circular links, then use the same type
+// - if not, choose the type with fewer links
+function selectCircularLinkTypes(graph, id) {
+  var numberOfTops = 0;
+  var numberOfBottoms = 0;
+  graph.links.forEach(function (link) {
+    if (link.circular) {
+      // if either souce or target has type already use that
+      if (link.source.circularLinkType || link.target.circularLinkType) {
+        // default to source type if available
+        link.circularLinkType = link.source.circularLinkType ? link.source.circularLinkType : link.target.circularLinkType;
+      } else {
+        link.circularLinkType = numberOfTops < numberOfBottoms ? 'top' : 'bottom';
+      }
+
+      if (link.circularLinkType == 'top') {
+        numberOfTops = numberOfTops + 1;
+      } else {
+        numberOfBottoms = numberOfBottoms + 1;
+      }
+
+      graph.nodes.forEach(function (node) {
+        if (getNodeID(node, id) == getNodeID(link.source, id) || getNodeID(node, id) == getNodeID(link.target, id)) {
+          node.circularLinkType = link.circularLinkType;
+        }
+      });
+    }
+  });
+
+  //correct self-linking links to be same direction as node
+  graph.links.forEach(function (link) {
+    if (link.circular) {
+      //if both source and target node are same type, then link should have same type
+      if (link.source.circularLinkType == link.target.circularLinkType) {
+        link.circularLinkType = link.source.circularLinkType;
+      }
+      //if link is self-linking, then link should have same type as node
+      if (selfLinking(link, id)) {
+        link.circularLinkType = link.source.circularLinkType;
+      }
+    }
+  });
+}
+
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) {
   return typeof obj;
 } : function (obj) {
@@ -1392,51 +1437,6 @@ function identifyCircles(graph, id) {
     } else {
       link.circular = false;
       addedLinks.push(link);
-    }
-  });
-}
-
-// Assign a circular link type (top or bottom), based on:
-// - if the source/target node already has circular links, then use the same type
-// - if not, choose the type with fewer links
-function selectCircularLinkTypes(graph, id) {
-  var numberOfTops = 0;
-  var numberOfBottoms = 0;
-  graph.links.forEach(function (link) {
-    if (link.circular) {
-      // if either souce or target has type already use that
-      if (link.source.circularLinkType || link.target.circularLinkType) {
-        // default to source type if available
-        link.circularLinkType = link.source.circularLinkType ? link.source.circularLinkType : link.target.circularLinkType;
-      } else {
-        link.circularLinkType = numberOfTops < numberOfBottoms ? 'top' : 'bottom';
-      }
-
-      if (link.circularLinkType == 'top') {
-        numberOfTops = numberOfTops + 1;
-      } else {
-        numberOfBottoms = numberOfBottoms + 1;
-      }
-
-      graph.nodes.forEach(function (node) {
-        if (getNodeID(node, id) == getNodeID(link.source, id) || getNodeID(node, id) == getNodeID(link.target, id)) {
-          node.circularLinkType = link.circularLinkType;
-        }
-      });
-    }
-  });
-
-  //correct self-linking links to be same direction as node
-  graph.links.forEach(function (link) {
-    if (link.circular) {
-      //if both source and target node are same type, then link should have same type
-      if (link.source.circularLinkType == link.target.circularLinkType) {
-        link.circularLinkType = link.source.circularLinkType;
-      }
-      //if link is self-linking, then link should have same type as node
-      if (selfLinking(link, id)) {
-        link.circularLinkType = link.source.circularLinkType;
-      }
     }
   });
 }
--- src/sankeyCircular.js
+++ src/sankeyCircular.js
@@ -23,6 +23,7 @@ import sortSourceLinks from './sortSourceLinks'
 import resolveNodeLinkOverlaps from './resolveNodeLinkOverlaps'
 import numberOfNonSelfLinkingCycles from './numberOfNonSelfLinkingCycles'
 import createsCycle from './createsCycle'
+import selectCircularLinkTypes from './selectCircularLinkTypes'
 
 // sort links' breadth (ie top to bottom in a column), based on their source nodes' breadths
 function ascendingSourceBreadth(a, b) {
@@ -635,57 +636,6 @@ function identifyCircles(graph, id) {
     } else {
       link.circular = false
       addedLinks.push(link)
-    }
-  })
-}
-
-// Assign a circular link type (top or bottom), based on:
-// - if the source/target node already has circular links, then use the same type
-// - if not, choose the type with fewer links
-function selectCircularLinkTypes(graph, id) {
-  var numberOfTops = 0
-  var numberOfBottoms = 0
-  graph.links.forEach(function(link) {
-    if (link.circular) {
-      // if either souce or target has type already use that
-      if (link.source.circularLinkType || link.target.circularLinkType) {
-        // default to source type if available
-        link.circularLinkType = link.source.circularLinkType
-          ? link.source.circularLinkType
-          : link.target.circularLinkType
-      } else {
-        link.circularLinkType =
-          numberOfTops < numberOfBottoms ? 'top' : 'bottom'
-      }
-
-      if (link.circularLinkType == 'top') {
-        numberOfTops = numberOfTops + 1
-      } else {
-        numberOfBottoms = numberOfBottoms + 1
-      }
-
-      graph.nodes.forEach(function(node) {
-        if (
-          getNodeID(node, id) == getNodeID(link.source, id) ||
-          getNodeID(node, id) == getNodeID(link.target, id)
-        ) {
-          node.circularLinkType = link.circularLinkType
-        }
-      })
-    }
-  })
-
-  //correct self-linking links to be same direction as node
-  graph.links.forEach(function(link) {
-    if (link.circular) {
-      //if both source and target node are same type, then link should have same type
-      if (link.source.circularLinkType == link.target.circularLinkType) {
-        link.circularLinkType = link.source.circularLinkType
-      }
-      //if link is self-linking, then link should have same type as node
-      if (selfLinking(link, id)) {
-        link.circularLinkType = link.source.circularLinkType
-      }
     }
   })
 }
